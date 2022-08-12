dc49ff1136a40116b399486df32425b4ef0570e4
joel <joelkalema63@gmail.com>
2022-08-12 21:50:07 +0200

    update content

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,7 +1,12 @@
 class Transaction < ApplicationRecord
-    belongs_to :user
-    belongs_to :category
-  
-    validates :name, presence: true
-    validates :amount, presence: true
+  belongs_to :user
+  belongs_to :category
+
+  validates :name, presence: true
+  validates :amount, presence: true
+
+  def total_amount
+    category.sum('amount')
   end
+  
+end
