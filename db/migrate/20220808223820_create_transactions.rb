00faf6821264016b2b571e95bd7114286f44a179
joel <joelkalema63@gmail.com>
2022-08-12 22:18:12 +0200

    fix bug

--- db/migrate/20220808223820_create_transactions.rb
+++ db/migrate/20220808223820_create_transactions.rb
@@ -1,7 +1,7 @@
 class CreateTransactions < ActiveRecord::Migration[7.0]
   def change
     create_table :transactions do |t|
-      t.string :amount
+      t.decimal :amount
       t.string :name
       t.references :user, index: true, foreign_key: true
       t.references :category, index: true, foreign_key: true
--- db/schema.rb
+++ db/schema.rb
@@ -24,7 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_09_142053) do
   end
 
   create_table "transactions", force: :cascade do |t|
-    t.string "amount"
+    t.decimal "amount"
     t.string "name"
     t.bigint "user_id"
     t.bigint "category_id"
